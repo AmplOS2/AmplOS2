2f33009a3f38ad9e31aaead736205d9a0a256ee4
chris <chrissx@chrissx.de>
2021-05-11 20:09:58 +0200

    of course the script is not interactive

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ tag=amplos-$target-buildenv
 shift
 docker images | grep amplos-raspi-buildenv >/dev/null || \
     docker build buildenvs -f buildenvs/$target.Dockerfile -t $tag
-docker run --rm -it -v $PWD:/amplos $tag sm $@
+docker run --rm -v $PWD:/amplos $tag sm $@
