59423ff163669ca95e3b1b9138277edf62d8b2f6
chris <chrissx@chrissx.de>
2021-05-16 05:27:17 +0200

    Fixed a bug in the buildenv.sh
    Here's a funny story: I found this while explaining the script to a
    friend at school on Friday.

--- buildenv.sh
+++ buildenv.sh
@@ -2,6 +2,6 @@
 target=$1
 tag=amplos-$target-buildenv
 shift
-docker images | grep amplos-raspi-buildenv >/dev/null || \
+docker images | grep $target >/dev/null || \
     docker build buildenvs -f buildenvs/$target.Dockerfile -t $tag
 docker run --rm -it -v $PWD:/amplos $tag
