bcffd38408a9e74f81291d4a90ce650576fba837
chris <chrissx@chrissx.de>
2021-06-06 22:47:01 +0200

    made `mmu_enable` actually work

--- kernel/raspi/mmu.hh
+++ kernel/raspi/mmu.hh
@@ -7,4 +7,4 @@
 typedef uint64_t mmu_table_descriptor;
 typedef uint64_t mmu_block_entry;
 
-extern "C" void mmu_enable(void *TTBR0_EL1, void *TTBR1_EL1, void *TCR_EL1);
+extern "C" void mmu_enable(void *ttbr0, void *ttbr1, void *tcr);
