AmplOS2/AmplOS2

commit f2eb8ac906ffe41dc7dbe34fa7f014b4e5e4a59e

general improvements to the code quality
code simplifications, removement of unnecessary specifiers, etc.

kernel/pages.cc
@@ -13,28 +13,18 @@ struct {
         bool allocated = false, continued = false;
 } pages[PAGE_COUNT];
 
-inline bool k_pages_viable(size_t first, size_t count) {
-        if(first + count >= PAGE_COUNT) return false;
-        for(size_t i = first; i < first + count; i++)
-                if(pages[i].allocated) return false;
-        return true;
-}
-
-inline void *k_pages_alloc_unchecked(size_t first, size_t count) {
-        for(size_t i = first; i < first + count - 1; i++) {
-                pages[i].allocated = true;
-                pages[i].continued = true;
-        }
-        pages[first + count - 1].allocated = true;
-        pages[first + count - 1].continued = false;
-        void *ptr                          = PAGE2ADDR(first);
-        memset(ptr, 0, PAGES2BYTES(count));
-        return ptr;
-}
-
 void *k_page_alloc(size_t count) {
-        for(size_t i = FIRST_PAGE; i < PAGE_COUNT; i++)
-                if(k_pages_viable(i, count)) return k_pages_alloc_unchecked(i, count);
+        for(size_t i = FIRST_PAGE; i < PAGE_COUNT - count; i++) {
+                if(i + count >= PAGE_COUNT) continue;
+                for(size_t j = i; j < i + count; j++)
+                        if(pages[j].allocated) continue;
+                for(size_t j = i; j < i + count; j++)
+                        pages[j].allocated = pages[j].continued = true;
+                pages[i + count - 1].continued = false;
+                void *ptr                      = PAGE2ADDR(i);
+                memset(ptr, 0, PAGES2BYTES(count));
+                return ptr;
+        }
         return NULL;
 }
 
@@ -42,6 +32,6 @@ void k_page_free(void *ptr) {
         if((uintptr_t)ptr % PAGE_SIZE) return;
         size_t i = ADDR2PAGE(ptr);
         if(!pages[i--].allocated) return;
-        while(pages[++i].continued) { pages[i].allocated = pages[i].continued = false; }
+        while(pages[++i].continued) pages[i].allocated = pages[i].continued = false;
         pages[i].allocated = false;
 }

.clang-format
@@ -10,7 +10,6 @@ AlignConsecutiveMacros: true
 AllowAllArgumentsOnNextLine: false
 AllowAllConstructorInitializersOnNextLine: false
 AllowAllParametersOfDeclarationOnNextLine: false
-AllowShortBlocksOnASingleLine: Always
 AllowShortCaseLabelsOnASingleLine: true
 AllowShortIfStatementsOnASingleLine: Always
 AllowShortLambdasOnASingleLine: All

kernel/graphics.hh
@@ -29,9 +29,10 @@ public:
         }
 
         // NOTE: breaks if other is smaller than this
-        constexpr inline void copy_to(buffer &other) const {
+        void copy_to(buffer &other) const {
                 // omg i hate clang rn, i really want to make this the code below
                 // (it is better in every way) but then it is about 100 times slower
+                // even if we're using templates which should make it actually constexpr und schnell
                 for(uint32_t i = 0; i < w * h; i++) other.b[i] = b[i];
                 // for(uint32_t x = 0; x < w; x++)
                 //        for(uint32_t y = 0; y < h; y++) other.pixel(x, y) = pixel(x, y);

kernel/raspi/hwinfo.hh
@@ -1,6 +1,8 @@
 #pragma once
 #include <stdint.h>
 
+namespace {
+
 // TODO: read a lot of docs and figure out wheather the isb is actually needed
 // TODO: figure out if there's any way we can make using this a one-liner
 #define mrs_to_var(reg, var) asm volatile("isb; mrs %0, " reg : "=r"(var))
@@ -17,45 +19,45 @@ public:
 
         // https://developer.arm.com/documentation/ddi0595/2020-12/AArch64-Registers/ID-AA64MMFR0-EL1--AArch64-Memory-Model-Feature-Register-0
 
-        constexpr inline bool ecv() const { return (mmfr0 >> 60) & 3; }
-        constexpr inline bool fgt() const { return ((mmfr0 >> 56) & 0xf) == 1; }
-        constexpr inline bool exs() const { return ((mmfr0 >> 44) & 0xf) == 1; }
+        inline bool ecv() const { return (mmfr0 >> 60) & 3; }
+        inline bool fgt() const { return ((mmfr0 >> 56) & 0xf) == 1; }
+        inline bool exs() const { return ((mmfr0 >> 44) & 0xf) == 1; }
 
-        constexpr inline bool stage1_tgran4() const { return ((mmfr0 >> 28) & 0xe) == 0; }
-        constexpr inline bool stage1_tgran16() const {
+        inline bool stage1_tgran4() const { return ((mmfr0 >> 28) & 0xe) == 0; }
+        inline bool stage1_tgran16() const {
                 uint32_t i = (mmfr0 >> 20) & 0xf;
                 return i == 1 || i == 2;
         }
-        constexpr inline bool stage1_tgran64() const { return ((mmfr0 >> 24) & 0xf) == 0; }
+        inline bool stage1_tgran64() const { return ((mmfr0 >> 24) & 0xf) == 0; }
 
-        constexpr inline bool stage2_tgran4() const {
+        inline bool stage2_tgran4() const {
                 uint32_t i = (mmfr0 >> 40) & 0xf;
                 if(i == 0) return stage1_tgran4();
                 return (i & 2) == 2;
         }
-        constexpr inline bool stage2_tgran16() const {
+        inline bool stage2_tgran16() const {
                 uint32_t i = (mmfr0 >> 32) & 0xf;
                 if(i == 0) return stage1_tgran16();
                 return (i & 2) == 2;
         }
-        constexpr inline bool stage2_tgran64() const {
+        inline bool stage2_tgran64() const {
                 uint32_t i = (mmfr0 >> 36) & 0xf;
                 if(i == 0) return stage1_tgran64();
                 return i == 2;
         }
 
-        constexpr inline bool mixed_endian() const { return ((mmfr0 >> 8) & 0xf) == 1; }
-        constexpr inline bool sns_mem() const { return ((mmfr0 >> 12) & 0xf) == 1; }
-        constexpr inline bool mixed_endian_el0() const {
+        inline bool mixed_endian() const { return ((mmfr0 >> 8) & 0xf) == 1; }
+        inline bool sns_mem() const { return ((mmfr0 >> 12) & 0xf) == 1; }
+        inline bool mixed_endian_el0() const {
                 return mixed_endian() || ((mmfr0 >> 16) & 0xf) == 1;
         }
-        constexpr inline int32_t asid_bits() const {
+        inline int32_t asid_bits() const {
                 uint32_t i = (mmfr0 >> 4) & 0xf;
                 if(i == 0) return 8;
                 if(i == 2) return 16;
                 return -1;
         }
-        constexpr inline int32_t pa_range() const {
+        inline int32_t pa_range() const {
                 switch(mmfr0 & 0xf) {
                         case 0: return 32;
                         case 1: return 36;
@@ -71,21 +73,19 @@ public:
         // TODO: other registers
 };
 
-namespace {
-static inline uint64_t rdtsc() {
+inline uint64_t rdtsc() {
         uint64_t i;
-        mrs_to_var("CNTVCT_EL0", i);
+        mrs_to_var("CNTPCT_EL0", i); // CNTPCT is the physical counter, CNTVCT the virtual one
         return i;
 }
 
-// NOTE: this is not always accurate
-static inline uint32_t cpufrequency() {
+inline uint32_t cpufrequency() {
         uint64_t i;
         mrs_to_var("CNTFRQ_EL0", i);
         return i & 0xffffffff;
 }
 
-static inline uint_fast8_t current_el() {
+inline uint_fast8_t current_el() {
         uint64_t i;
         mrs_to_var("CurrentEL", i);
         return (i & 0b1100) >> 2;

kernel/raspi/mbox.hh
@@ -36,7 +36,7 @@
 extern volatile uint32_t mbox[36];
 
 namespace {
-static inline bool mbox_call(const char ch) {
+inline bool mbox_call(const char ch) {
         // static_assert(ch < 0x10);
         // static_assert((((uintptr_t)&mbox) & 0xffffffff0000000f) == 0);
         uint32_t r = (((uint32_t)(uintptr_t)&mbox) | ch);
@@ -51,18 +51,18 @@ static inline bool mbox_call(const char ch) {
         }
 }
 
-static constexpr inline size_t mbox_start(size_t len) {
+constexpr inline size_t mbox_start(size_t len) {
         size_t idx  = 0;
         mbox[idx++] = len * sizeof(uint32_t);
         mbox[idx++] = MBOX_REQUEST;
         return idx;
 }
 
-static constexpr inline void mbox_cmd(size_t &       idx,
-                                      const uint32_t cmd,
-                                      const uint32_t len,
-                                      const uint32_t arg1,
-                                      const uint32_t arg2 = 0) {
+constexpr inline void mbox_cmd(size_t &       idx,
+                               const uint32_t cmd,
+                               const uint32_t len,
+                               const uint32_t arg1,
+                               const uint32_t arg2 = 0) {
         // static_assert(len == 1 || len == 2);
         mbox[idx++] = cmd;
         mbox[idx++] = len * sizeof(uint32_t);
@@ -71,5 +71,5 @@ static constexpr inline void mbox_cmd(size_t &       idx,
         if(len == 2) mbox[idx++] = arg2;
 }
 
-static constexpr inline void mbox_end(size_t &idx) { mbox[idx++] = MBOX_TAG_LAST; }
+constexpr inline void mbox_end(size_t &idx) { mbox[idx++] = MBOX_TAG_LAST; }
 }

kernel/raspi/mmu.hh
@@ -1,10 +1,14 @@
 #include <stdint.h>
 
-// TODO: read ID_AA64MMFR0_EL1
-#define GRANULE_SIZE 64 * 1024
+namespace MMU {
+
+// TODO: read ID_AA64MMFR0
+const unsigned GRANULE_SIZE = 64 * 1024;
 
 // TODO: make it easy to create them
-typedef uint64_t mmu_table_descriptor;
-typedef uint64_t mmu_block_entry;
+typedef uint64_t table_descriptor;
+typedef uint64_t block_entry;
 
 extern "C" void mmu_enable(void *ttbr0, void *ttbr1, void *tcr);
+
+};

kernel/raspi/timing_and_power.hh
@@ -17,14 +17,14 @@
 #define PM_RSTC_FULLRST 0x00000020
 
 namespace {
-inline void cpu_relax() {
-        // this is copy-paste from linux
-        // (i have actually looked up what arm says, yield
-        //  is for, and using it seems fine to me)
-        asm volatile("yield" ::: "memory");
-}
-constexpr static inline void spincycles(uint64_t n) { spinwhile(n--); }
-static inline void           shutdown(bool restart) {
+// this is copy-paste from linux
+// (i have actually it looked up from arm and it seems fine to me)
+#define cpu_relax() asm volatile("yield" ::: "memory")
+
+inline void spincycles(uint64_t n) { spinwhile(n--); }
+
+#define abort shutdown
+void shutdown(bool restart = false) {
         if(!restart) {
                 for(uint32_t i = 0; i < 16; i++) {
                         size_t idx = mbox_start(8);
@@ -54,14 +54,12 @@ static inline void           shutdown(bool restart) {
         PM_WDOG = PM_WDOG_MAGIC | 10;
         PM_RSTC = PM_WDOG_MAGIC | PM_RSTC_FULLRST;
 }
-}
 
-namespace {
-static inline uint64_t clock() { return ((uint64_t)SYSTMR_HI << 32) | SYSTMR_LO; }
-static inline void     usleep(uint64_t n) {
+inline uint64_t clock() { return ((uint64_t)SYSTMR_HI << 32) | SYSTMR_LO; }
+
+inline void usleep(uint64_t n) {
         uint64_t t = clock() + n;
         if(t == n) return;
         spinwhile(clock() < t);
 }
-static inline void abort() { shutdown(false); }
 }

commit d2c69dbdbe9c77af91a1ae40829c30591b989d6c

Fixed a really stupid bug that broke `kalloc`
introduced in f2eb8ac906ffe41dc7dbe34fa7f014b4e5e4a59e

kernel/main.cc
@@ -77,8 +77,8 @@ extern "C" void kmain() {
                                rgba(rand() & f, rand() & f, rand() & f, 0);
                 });
                 for(uint32_t x = 0; x < gpu.width(); x++) {
-                        uint32_t y = sin(fmod(((double)x) / 100, 6.3)) * 200 - 100 +
-                                     gpu.width() / 2;
+                        uint32_t y =
+                                sin(fmod(((double)x) / 100, 6.3)) * 200 - 100 + gpu.width() / 2;
                         for(uint32_t i = y - 5; i <= y + 5; i++) gpu.pixel(x, i) = 0xffffffff;
                 }
                 gpu.draw_text_unifont("AmplOS2 is the best operating system ever created by "

kernel/pages.cc
@@ -16,8 +16,9 @@ struct {
 void *k_page_alloc(size_t count) {
         for(size_t i = FIRST_PAGE; i < PAGE_COUNT - count; i++) {
                 if(i + count >= PAGE_COUNT) continue;
-                for(size_t j = i; j < i + count; j++)
-                        if(pages[j].allocated) continue;
+                bool b;
+                for(size_t j = i; j < i + count; j++) b |= pages[j].allocated;
+                if(b) continue;
                 for(size_t j = i; j < i + count; j++)
                         pages[j].allocated = pages[j].continued = true;
                 pages[i + count - 1].continued = false;